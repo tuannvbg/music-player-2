8121ab78efbc884f366c3b0ec017dc2933667596
Rafael Almeida <rafael.almeida@tripda.com.br>
2014-12-11 18:44:12 -0200

    Configure Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
-var gulp = require('gulp'),
+(function () {
+    'use strict';
+
+    var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
     jshint = require('gulp-jshint');
 
-// JSHint
-gulp.task('lint', function() {
-    return gulp.src()
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
+    // JSHint
+    /*gulp.task('lint', function() {
+        return gulp.src()
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+    });*/
+
+    // Uglify
+    gulp.task('compress', function() {
+      gulp.src(['app/app.js', 'app/config.js', 'app/**/*.js'])
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js'));
+    });
+
+})();
